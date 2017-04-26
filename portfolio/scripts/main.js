d60ea59e9fff84cbf988e195e527a0afaea4698c
fujisawakyle <fujisawakyle@gmail.com>
2017-04-25 20:55:38 -0700

    left banner implementation

--- portfolio/scripts/main.js
+++ portfolio/scripts/main.js
@@ -1,31 +1,18 @@
 $(document).ready(function ( ) {
 	
 	var menu = document.querySelector('#menu');
-	var menuClose =
-	document.querySelector('#menuClose')
-  var main = document.querySelector('main');
 	var drawer = document.querySelector('.nav');
 	
 	menu.addEventListener('click', function(e) {
-        drawer.classList.toggle('open');
-				main.classList.toggle('open');
-				e.stopPropagation();
-				$(".menuButton").css("display","none");
-      });
-      menuClose.addEventListener('click', function() {
-      	drawer.classList.remove('open');
-				main.classList.remove('open');
-				$(".menuButton").css("display","inline-block");
+  
+		drawer.classList.toggle('open');
+		e.stopPropagation();
+  
+	});
+	menuClose.addEventListener('click', function() {
+  
+		drawer.classList.remove('open');
 				
-      });
+ 	});
 	
-   /* $("#menu").on("click", function (e) {
-			$('.nav').classList.toggle('open');
-			e.stopPropagation();
-			$('#homeSection').html('yooo');
-		});
-	
-		$('main').on('click', function () {
-			$('#drawer').classList.remove('open');
-		});*/
 })
